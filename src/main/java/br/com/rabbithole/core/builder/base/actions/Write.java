bbaccf684d96d1d70aa7cb7ea685d8f6a5741e83
Felipe Ros <feiperos.simao@gmail.com>
2023-05-05 06:01:13 -0300

    initial implementation of javadoc

--- src/main/java/br/com/rabbithole/core/builder/base/actions/Write.java
+++ src/main/java/br/com/rabbithole/core/builder/base/actions/Write.java
@@ -1,9 +1,11 @@
 package br.com.rabbithole.core.builder.base.actions;
 
 /**
- * @author Gabriel Fekete
- * @Usage Representação da Ação Read no banco de dados Redis
+ * @author Felipe Ros
+ * @Usage Representação da Ação Read no banco de dados Redis.
  * @since 2.0
+ * @version 1.0
+ * @param <Q> Genérico - Valor genérico utilizado no método getValue()
  */
 public interface Write<Q> {
     String getKey();
--- src/main/java/br/com/rabbithole/core/builder/base/options/CommandOptions.java
+++ src/main/java/br/com/rabbithole/core/builder/base/options/CommandOptions.java
@@ -1,5 +1,12 @@
 package br.com.rabbithole.core.builder.base.options;
 
+/**
+ * @author Felipe Ros
+ * @Usage Representação de opções dos comandos Redis.
+ * @since 2.0
+ * @version 1.0
+ * @param <T> Genérico - Valor genérico que estende da interface {@link Options}
+ */
 public interface CommandOptions<T extends Options> {
     T getOptions();
 }
