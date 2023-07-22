e68f9b3c34c6fd7f38c1ac74bccd4f603490dbdc
= <feliperos.simao@gmail.com>
2023-07-22 19:47:47 -0300

    update(options): turning obsolete options

--- src/main/java/br/com/rabbithole/core/builder/base/options/Options.java
+++ src/main/java/br/com/rabbithole/core/builder/base/options/Options.java
@@ -6,5 +6,6 @@ package br.com.rabbithole.core.builder.base.options;
  * @since 2.0
  * @version 1.0
  */
+@Deprecated
 public interface Options {
 }
--- src/main/java/br/com/rabbithole/core/builder/options/HashSetOptions.java
+++ src/main/java/br/com/rabbithole/core/builder/options/HashSetOptions.java
@@ -3,5 +3,6 @@ package br.com.rabbithole.core.builder.options;
 import br.com.rabbithole.core.builder.base.options.Options;
 
 //TODO: IMPLEMENTAR DPS!
+@Deprecated
 public class HashSetOptions implements Options {
 }
