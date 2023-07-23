48f62d9f37f9685a7c387dabc18084e31e47eb22
= <feliperos.simao@gmail.com>
2023-07-23 06:45:07 -0300

    implements object commands

---
+++ src/main/java/br/com/rabbithole/core/builder/commands/generics/object/ObjectEncoding.java
@@ -0,0 +1,68 @@
+package br.com.rabbithole.core.builder.commands.generics.object;
+
+import br.com.rabbithole.RedisLib;
+import br.com.rabbithole.core.builder.Query;
+import br.com.rabbithole.core.builder.base.Command;
+import br.com.rabbithole.core.builder.base.Execute;
+import br.com.rabbithole.core.builder.base.actions.Read;
+import redis.clients.jedis.Jedis;
+
+import java.util.Optional;
+
+/**
+ * @author Felipe Ros
+ * @Usage Retorna a codificação interna do objeto armazenado
+ * @since 2.0
+ * @version 1.0
+ */
+public class ObjectEncoding implements Command, Read, Execute<String> {
+    private final String key;
+
+    @Override
+    public String commandName() {
+        return "objectEncoding";
+    }
+
+    @Override
+    public String getKey() {
+        return this.key;
+    }
+
+    @Override
+    public Optional<String> execute() {
+        try (Jedis jedis = RedisLib.getJedis().getResource()) {
+            if (RedisLib.inDebug()) RedisLib.getLogger().info("Query: " + commandName() + " has executed!");
+            String result = jedis.objectEncoding(getKey());
+            return (result == null ? Optional.empty() : Optional.of(result));
+        } catch (Exception exception) {
+            RedisLib.getLogger().error("Query: " + commandName(), exception);
+            return Optional.empty();
+        }
+    }
+
+    private ObjectEncoding(Builder builder) {
+        this.key = builder.key;
+    }
+
+    private Query<ObjectEncoding> query() {
+        return new Query<>(this);
+    }
+
+    public static class Builder implements Execute<String> {
+        private String key;
+
+        public Builder setKey(String key) {
+            this.key = key;
+            return this;
+        }
+
+        public Query<ObjectEncoding> build() {
+            return new ObjectEncoding(this).query();
+        }
+
+        @Override
+        public Optional<String> execute() {
+            return build().getCommand().execute();
+        }
+    }
+}
